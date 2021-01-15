a867a7022073eaf3fa1e6e1c83cc3a5a1881fa82
Jacob Hickman <strivelinkband@yahoo.com>
2021-01-15 01:11:25 -0600

    Second Commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
+const { builtinModules } = require('module');
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
+const pokemonList = [];
+
 async function getPokemon() {
-  const pokemonList = [];
   for (let i = 1; i < 20; i++) {
     const pokemon = await axios.get(`${BASE_URL}/${i}`);
     const { data } = pokemon;
-    const { name, id, height, weight, stats, sprites } = data;
+    const { name, id, height, weight, stats, sprites, base_experience } = data;
     const statNames = [];
     // console.log(data);
     stats.forEach(pokeStat => {
@@ -18,6 +20,7 @@ async function getPokemon() {
     let pokemonInfo = {
       'name': name,
       'id': id,
+      'baseExperience': base_experience,
       'height': height,
       'weight': weight,
       'frontImage': sprites.front_default,
@@ -30,12 +33,14 @@ async function getPokemon() {
 }
 
 function getPokemonInfo() {
-  return axios.get(`${BASE_URL}/1`).then(({ data }) => console.log(data.sprites.front_default));
+  return axios.get(`${BASE_URL}/1`).then(({ data }) => console.log(data));
 }
-async function addPokemonToWebsite(){
-  const pokemons = getPokemon();
-  pokemons.forEach(pokemon => {
-    document.getElementById('pokemon-name').innerHTML = pokemon.name;
-  });
+function addPokemonToWebsite(){
+  document.getElementById('pokemon-name').innerHTML = pokemonList[0].name;
+  // document.getElementById('pokemon-name').innerHTML = pokemonList[0].name;
 }
+
 getPokemon();
+addPokemonToWebsite();
+
+module.exports = { addPokemonToWebsite, getPokemon };
